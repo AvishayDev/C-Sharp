d5130e9526be99050f5a59cd17cf066f913d742c
AvishayDev <62137674+AvishayDev@users.noreply.github.com>
2022-04-04 22:49:26 +0300

    read API fast and easy

---
+++ APiReading/ApiClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsProject.Classes.APiReading
+{
+    public static class ApiClient
+    {
+        public static HttpClient HttpClient { get; set; }
+
+        public static void InitializeHttpClient()
+        {
+            HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ APiReading/JsonReaderAPI.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsProject.Classes;
+
+namespace WindowsProject.Classes.APiReading
+{
+
+    public class JsonReaderAPi
+    {
+
+        protected string JsonQuary = "";
+
+        protected readonly string APIKey;
+
+        public JsonReaderAPi(string ApiKey)
+        {
+            APIKey = ApiKey;
+        }
+
+        protected virtual void AddDefaultParameters() { }
+
+
+        public virtual void AddParameter(string parameterName, object parameterValue)
+        {
+            JsonQuary += $"{parameterName}={parameterValue}&";
+        }
+
+        public virtual void AddQuary(string Quary)
+        {
+            JsonQuary += Quary + "?";
+        }
+
+        public async Task<T> ReadJson<T>(string JsonQuary = "", IJsonDeserializer<T> jsonDeserialize = null)
+        {
+            if (JsonQuary != "")
+                this.JsonQuary = JsonQuary;
+            else
+                AddDefaultParameters();
+
+            if (!CanRead())
+                return default;
+
+
+            // TODO: add cancel option after 10 sec
+            // TODO: try-catch for all and delete "CanRead"
+
+            // ask for Json
+            var Json = await ApiClient.HttpClient.GetStringAsync(this.JsonQuary);
+
+            //reset the json for next call
+            this.JsonQuary = "";
+
+            //return final variable
+            return jsonDeserialize == null ? JsonConvert.DeserializeObject<T>(Json) : jsonDeserialize.Deserialize(Json);
+        }
+
+        
+
+        private bool CanRead()
+        {
+            return JsonQuary.Contains("https://") || JsonQuary.Contains("http://");
+        }
+
+    }
+}
